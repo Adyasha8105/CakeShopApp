c7d6263abc616b63ba600b64c81bf6df8ebddfa1
adyasha <adyashamohanty8105@gmail.com>
2021-06-12 21:21:54 +0530

    Added actions

---
+++ src/components/CakeContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function CakeContainer(props) {
+  return (
+    <div>
+      <h2>Number of cakes</h2>
+      <button>Buy Cake</button>
+    </div>
+  );
+}
+
+export default CakeContainer;
---
+++ src/redux/cakeActions.js
@@ -0,0 +1,7 @@
+import { BUY_CAKE } from "./cakeTypes"
+
+const buyCake = () => {
+  return {
+    type: BUY_CAKE
+  }
+}
