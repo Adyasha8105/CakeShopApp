e582c832acbca9c26511e0c774a9dba26bf7f708
adyasha <adyashamohanty8105@gmail.com>
2021-06-13 00:46:41 +0530

    logger middleware functionality

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,9 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 // import cakeReducer from "./cake/cakeReducer";
 import rootReducers from "./rootReducer";
+import logger from "redux-logger"
 
 // const store = createStore(cakeReducer)
-const store = createStore(rootReducers)
+const store = createStore(rootReducers, applyMiddleware(logger))
 
 export default store
