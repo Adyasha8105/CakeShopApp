8a0fafd9a05d272167cbfc31bfc9f8241252512d
adyasha <adyashamohanty8105@gmail.com>
2021-06-13 00:19:21 +0530

    ADD dispatch method

--- src/components/HooksCakeConatiner.js
+++ src/components/HooksCakeConatiner.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { buyCake } from '../redux';
 
 function HooksCakeConatiner() {
   const numOfCakes = useSelector(state => state.numOfCakes)
-
+  const dispatch = useDispatch()
   return (
     <div>
       <h2>Num of Cakes - {numOfCakes}</h2>
-      <button>Buy cakes</button>
+      <button onClick={() => dispatch(buyCake())}>Buy cakes</button>
     </div>
   );
 }
---
+++ src/redux/IceCream/IceCreamActions.js
@@ -0,0 +1,7 @@
+import { BUY_CAKE } from "./IceCreamTypes"
+
+export const buyCake = () => {
+  return {
+    type: BUY_CAKE
+  }
+}
